7ff34e5a6df2c9d7f3f5661ac6a8f3513ecabdff
Keshav Lohani <139124045+keshav-1303@users.noreply.github.com>
2025-06-13 08:45:39 +0530

    Update Tours.js

--- components/Tours.js
+++ components/Tours.js
@@ -4,7 +4,7 @@ function Tours({tours, removeTour}) {
     return (
         <div className='container'>
             <div>
-                <h2 className='title'> Plan With Love</h2>
+                <h2 className='title'> Travel Planning</h2>
             </div>
             <div className='cards'>
                 {
